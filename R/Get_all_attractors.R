328ac24fda872d797d27db1d08fde8dfa4b6492a
Itziar91 <itzirurzun@alumni.unav.es>
2016-11-02 13:32:36 +0100

    Vignettes created

--- R/Get_all_attractors.R
+++ R/Get_all_attractors.R
@@ -1,31 +1,48 @@
 
-Get_all_attractor_wrapper.f=function(cpus,BN,time.steps,asynchronous,repetitions,r,combinations){
-
+Get_all_attractor_wrapper.f=function(cpus,BN,time.steps,asynchronous,repetitions,r,combinations,Percent.ON){
   Attr<-list()
+  Attr_states<-list()
   o=1
   r2<-r[(1+(cpus-1)*combinations):(cpus*combinations),]
   nodes.names<-rev(BN$nodes.names)
   for(i in 1:combinations){
     BN$Initial_conditions<-nodes.names[as.logical(r2[i,])]
-    Attractor<-SPIDDOR::Get_Attractor.f(BN,time.steps=time.steps,asynchronous=asynchronous,repetitions=repetitions,Percent.ON=TRUE)
+    if(asynchronous==FALSE & Percent.ON==FALSE){
+      Attractor_states<-Get_Attractor.f(BN,time.steps=time.steps,asynchronous=asynchronous,repetitions=repetitions,Percent.ON=FALSE)
+      if(length(Attractor_states)==length(BN$nodes.names)){
+        Attractor<-Attractor_states
+      }else{
+        Attractor<-apply(Attractor_states,2,sum)/dim(Attractor_states)[1]
+      }
+    }else{
+      Attractor<-Get_Attractor.f(BN,time.steps=time.steps,asynchronous=asynchronous,repetitions=repetitions,Percent.ON=TRUE)
+    }
     
     a<-lapply(Attr,function(x)x[1:length(BN$nodes.names)]/Attractor)
-    if(!is.na(all(lapply(a,function(x)which(any(x>=1.24 | x<=0.81)))==T))){
+    
+    if(!is.na(all(lapply(a,function(x)which(any(x>=1.25 | x<=0.8)))==T))){
+      if(asynchronous==FALSE & Percent.ON==FALSE){
+        Attr_states[[o]]<-Attractor_states
+      }
       Attr[[o]]<-c(round(Attractor,3),"Recurrence"=1)
       o=o+1
     }else{
-      Attr[[which(sapply(a,function(x)all(x<1.24 & x>0.81,na.rm=TRUE)))[1]]]["Recurrence"]<-Attr[[which(sapply(a,function(x)all(x<1.24 & x>0.81,na.rm=TRUE)))[1]]]["Recurrence"]+1
+      if(asynchronous==FALSE & Percent.ON==FALSE) next()
+      Attr[[which(sapply(a,function(x)all(x<1.25 & x>0.8,na.rm=TRUE)))[1]]]["Recurrence"]<-Attr[[which(sapply(a,function(x)all(x<1.25 & x>0.8,na.rm=TRUE)))[1]]]["Recurrence"]+1
     } 
   }
+  if(asynchronous==FALSE & Percent.ON==FALSE) return(Attr_states)
+  
   return(Attr)
 }
 
 #' @export
-Get_all_attractors.f=function(cpus,BN,asynchronous=FALSE,repetitions=0,startStates=NULL){
+Get_all_attractors.f=function(cpus,BN,asynchronous=FALSE,repetitions=0,startStates=NULL,Percent.ON=TRUE){
   
   snowfall::sfInit( parallel=TRUE, cpus=cpus)
   capture.output(snowfall::sfSource("dynamic_evolution.R"),file='NUL')
-
+  capture.output(snowfall::sfSource(paste(system.file("R",package="SPIDDOR"),"/Get_attractor.R",sep="")),file='NUL')
+  
   snowfall::sfClusterSetupRNGstream(seed=runif(1,min=0,max=9.22e+18))
   
   BN$Polymorphism<-setNames(rep(1,length(BN$nodes.names)),BN$nodes.names)
@@ -39,20 +56,29 @@ Get_all_attractors.f=function(cpus,BN,asynchronous=FALSE,repetitions=0,startStat
     else if(startStates>(2^length(BN$nodes.names))) startStates=2^length(BN$nodes.names)
     r<-r[1:startStates,]
   }
-
+  
+  
+  if(length(BN$nodes.names)<=25){time_steps=999}
+  else if(length(BN$nodes.names)>25 & length(BN$nodes.names)<=50){time_steps=1999}
+  else{time_steps=2999}
   
   if(repetitions>60 & asynchronous==TRUE) repetitions=60
   
   attr_i=snowfall::sfClusterApplyLB(1:cpus,Get_all_attractor_wrapper.f,
-                          BN,
-                          time.steps=999,
-                          asynchronous,
-                          repetitions,
-                          r,
-                          combinations=dim(r)[1]/cpus)
+                                    BN,
+                                    time.steps=time_steps,
+                                    asynchronous,
+                                    repetitions,
+                                    r,
+                                    combinations=dim(r)[1]/cpus,
+                                    Percent.ON)
   
   snowfall::sfStop()
   
+  if(asynchronous==FALSE & Percent.ON==FALSE){
+    attractors<-Reduce('unique',attr_i)
+    return(attractors)
+  } 
   a<-matrix(unlist(attr_i),ncol=length(BN$nodes.names)+1,byrow =T)
   colnames(a)<-c(BN$nodes.names,"Recurrence")
   DTT<-data.table::data.table(round(a,1))
@@ -82,8 +108,4 @@ last_check<-function(attractors,BN){
   return(attractors)
 }
 
-#EXAMPLES:
-#library(snowfall)
-#system.time((A<-Get_all_attractors.f(8,BN,asynchronous = FALSE,12)))
-#A2<-Get_all_attractors.f(8,BN,asynchronous = FALSE,12,startStates=1000)
-#B<-Get_all_attractors.f(8,BN,asynchronous = TRUE,36)
+
