ff3a016a935097e831b2ededd4c40180bdf7f261
Itziar91 <itzirurzun@alumni.unav.es>
2016-11-11 17:52:51 +0100

    heatmaps

--- R/Create_heatmap.R
+++ R/Create_heatmap.R
@@ -3,17 +3,13 @@
 Create_heatmap<-function(M,hclust.method="average",palette=NULL){
   #Matrix parametrization:
   mat_par.m<-Matrix_parametrization.f(M)
-  #Calculate distances using euclidean metric:
-  d<-dist(mat_par.m) 
-  #Average linkage strategy:
-  h<-hclust(d,method=hclust.method)
-  
   #Draw heatmap:
   if(length(palette)==0){
     mypalette<-c("#99CCFF","#0066CC","#000000","#FF6600","#FF9933")
   }
 
-  heatmap.2(mat_par.m,density.info=c("none"),trace=c("none"),dendrogram=c("col"),Colv=as.dendrogram(h),Rowv="NA",
-            col =mypalette,key=TRUE, symkey=TRUE,cexCol=1)
+  heatmap.2(mat_par.m, density.info = c("none"), trace = c("none"), 
+            dendrogram = c("col"), Rowv = "NA", hclustfun = function(x) hclust(x,method = hclust.method),
+            col = mypalette, key = TRUE, symkey = TRUE, cexCol = 1)
   
 }
